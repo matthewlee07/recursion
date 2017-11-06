d6795a973e5c9b92a84667b3fd2bfba67b7218b4
matthew lee <matthewlee07@gmail.com>
2017-11-06 16:41:03 -0500

    completed problem 6

--- recursion.js
+++ recursion.js
@@ -26,8 +26,6 @@ function doubleArray(array) {
     return [doubledIndex, ...doubleArray(array.slice(1))];
 }
 
-console.log(doubleArray([1, 2, 3]));
-
 //Reverse String
 function reverseString(string) {
     //base case
@@ -46,9 +44,58 @@ function triangularNumber(n) {
 }
 
 //String Splitter
+function splitString(string, separator) {
+    //base case: stop at end of string
+    if (!string.length) return '';
+    //recursive: charAt(0), sperator, splitString()
+    return string.charAt(0) + separator + splitString(string.slice(1), separator);
+}
 
-//Binary Representation
+// Binary Representation
+// function cutTwos(num) {
+//     if (num === 1) return "1";
 
+//     if (32 < num && num-32 !== 1) {
+//         return "1" + cutTwos(num - 32);
+//     }
+//     if (16 < num && num-16 !== 1) {
+//         return "1" + cutTwos(num - 16);
+//     }
+//     if (8 < num && num-8 !== 1) {
+//         return "1" + cutTwos(num - 8);
+//     }
+//     if (4 < num && num-4 !== 1) {
+//         return "1" + cutTwos(num - 4);
+//     }
+//     if (2 < num && num-2 !== 1) {
+//         return "1" + cutTwos(num - 2);
+//     }
+//     //define case for 0
+//     else {
+//         return "0" + cutTwos(num - 2);
+//     }
+// }
+
+// function binary(num) {
+//     if (num === 1) return 1;
+//     if (128 < num) { return 10000000 + (binary(num - 128)); }
+//     if (64 < num) { return 1000000 + (binary(num - 64)); }
+//     if (32 < num) { return 100000 + (binary(num - 32)); }
+//     if (16 < num) { return 10000 + (binary(num - 16)); }
+//     if (8 < num) { return 1000 + (binary(num - 8)); }
+//     if (4 < num) { return 100 + (binary(num - 4)); }
+//     if (2 < num) { return 10 + (binary(num - 16)); }
+// }
+
+function binary(num) {
+    // basecase
+    if (num < 1) return '';
+    // if num is even concat binary to 0 
+    if (num % 2 === 0) return binary(num / 2) + "0" ;
+    if (num % 2 !== 0) return binary(Math.floor(num / 2)) + "1";
+}
+
+console.log(binary(29));
 //Anagrams
 
 //Animal Hierarchy
